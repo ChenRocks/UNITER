87308d2fe2f6abfc743eb094dac18addb1a914fb
Yen-Chun Chen <Yen-Chun.Chen@microsoft.com>
2020-08-06 01:06:41 -0700

    ve dataset

--- data/__init__.py
+++ data/__init__.py
@@ -8,6 +8,7 @@ from .data import (TxtTokLmdb, DetectFeatLmdb,
 from .sampler import TokenBucketSampler
 from .loader import PrefetchLoader
 from .vqa import VqaDataset, VqaEvalDataset, vqa_collate, vqa_eval_collate
+from .ve import VeDataset, VeEvalDataset, ve_collate, ve_eval_collate
 from .nlvr2 import (Nlvr2PairedDataset, Nlvr2PairedEvalDataset,
                     Nlvr2TripletDataset, Nlvr2TripletEvalDataset,
                     nlvr2_paired_collate, nlvr2_paired_eval_collate,
